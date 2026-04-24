dadc2237bc1fe707a4c3a40e32c4c0ee87fb2f69
chols8195 <lisa.cho@ucdenver.edu>
2026-04-23 19:03:54 -0600

    setting up backend

---
+++ backend/main.py
@@ -0,0 +1,13 @@
+# backend/main.py
+from fastapi import FastAPI 
+from dotenv import load_dotenv
+from routers import pomodoro, canvas, logs
+
+load_dotenv()
+
+app = FastAPI(title="Kanban Board")
+
+app.include_router(pomodoro.router)
+app.include_router(canvas.router)
+app.include_router(logs.router)
+
